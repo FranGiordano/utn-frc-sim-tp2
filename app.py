83ea8cc7231151b54849501a301c651402493718
FranGiordano <francoomargiordano@hotmail.com>
2023-04-15 01:16:05 -0300

    commit grafico

--- app.py
+++ app.py
@@ -1,26 +1,76 @@
-from dash import Dash, dcc, html, Input, Output
-import os
+from dash import Dash, dcc, html, Input, Output, no_update, State
+import dash_bootstrap_components as dbc
+import soporte.simulacion as sim
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = Dash(__name__,
+           external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 server = app.server
 
-app.layout = html.Div([
-    html.H1('Hello World'),
-    dcc.Dropdown(['LA', 'NYC', 'MTL'],
-                 'LA',
-                 id='dropdown'
-                 ),
-    html.Div(id='display-value')
+app.layout = dbc.Container([
+    html.H1('Trabajo Práctico Número 2'),
+
+    dbc.Row([
+
+            dbc.Col([
+                dbc.FormFloating([
+                    dbc.Input(id="in_cantidad_muestras", placeholder="Cantidad de muestras", type="number", max=50000,
+                              min=0, step=1, value=10000, required=True),
+                    dbc.Label("Cantidad de muestras"),
+                ])]),
+
+            dbc.Col([
+                dbc.FormFloating([
+                    dbc.Input(id="in_limite_inferior", placeholder="Límite inferior", type="number",
+                              value=-10, required=True, step=0.0001),
+                    dbc.Label("Límite inferior"),
+                ])]),
+
+            dbc.Col([
+                dbc.FormFloating([
+                    dbc.Input(id="in_limite_superior", placeholder="Límite superior", type="number",
+                              value=10, required=True, step=0.0001),
+                    dbc.Label("Límite superior"),
+                ])]),
+
+            dbc.Col([
+                dbc.FormFloating([
+                    dbc.Input(id="in_intervalos", placeholder="Cantidad de intervalos", type="number", min=1, step=1,
+                              value=15, required=True, max=225),
+                    dbc.Label("Cantidad de intervalos"),
+                ])]),
+
+            dbc.Col(dbc.Button("Generar distribución", id="btn_cargar_grafico", color="primary"),
+                    class_name="col-auto align-self-end")
+
+    ]),
+
+    dcc.Graph(id="histograma", figure={})
 ])
 
 
-@app.callback(Output('display-value', 'children'),
-              [Input('dropdown', 'value')])
-def display_value(value):
-    return f'You have selected {value}'
+@app.callback(
+    Output('histograma', 'figure'),
+    State("in_cantidad_muestras", "value"),
+    State("in_limite_inferior", "value"),
+    State("in_limite_superior", "value"),
+    State("in_intervalos", "value"),
+    Input("btn_cargar_grafico", "n_clicks"),
+    prevent_initial_call=True
+)
+def generar_grafico(n, li, ls, intervalos, n_clicks):
+
+    if None in [n, li, ls, intervalos]:
+        return True, no_update
+
+    if float(li) > float(ls):
+        return True, no_update
+
+    serie = sim.generar_lista_uniforme(int(n), float(li), float(ls))
+
+    histograma = sim.generar_histograma(serie, intervalos)
+
+    return histograma
 
 
 if __name__ == '__main__':
---
+++ soporte/simulacion.py
@@ -0,0 +1,86 @@
+import random as rd
+import math
+import plotly.graph_objs as go
+import numpy as np
+
+
+def generar_lista_uniforme(n, a, b):
+    if a > b:
+        c = a
+        a = b
+        b = c
+    serie = []
+    for i in range(n):
+        x = a + (b - a) * rd.random()
+        serie.append(x)
+    return serie
+
+
+def generar_lista_exponencial_negativa(n, lam):
+    serie = []
+    for i in range(n):
+        x = -(1 / lam) * math.log(1 - rd.random())
+        serie.append(x)
+    return serie
+
+
+def generar_lista_normal(n, desv_est, media):
+    serie = []
+    i = 0
+    while i < n:
+        rd1 = rd.random()
+        rd2 = rd.random()
+        n1 = math.sqrt(-2 * math.log(rd1)) * math.cos(2 * math.pi * rd2) * desv_est + media
+        n2 = math.sqrt(-2 * math.log(rd1)) * math.sin(2 * math.pi * rd2) * desv_est + media
+        serie.append(n1)
+        serie.append(n2)
+        i += 2
+    if n % 2 == 1:
+        serie.pop()
+    return serie
+
+
+def generar_lista_poisson(n, lam):
+    serie = []
+    for i in range(n):
+        p = 1
+        x = -1
+        a = math.e ** (-lam)
+        while p >= a:
+            u = rd.random()
+            p *= u
+            x = x + 1
+        serie.append(x)
+    return serie
+
+
+def generar_histograma(muestras, n_barras):
+
+    # Constantes
+
+    maximo = max(muestras)
+    minimo = min(muestras)
+    rango = (maximo - minimo) / n_barras
+
+    # Creación de histograma
+
+    fig = go.Figure(
+        data=go.Histogram(
+            x=muestras,
+            xbins=dict(
+                start=minimo,
+                size=rango,
+                end=maximo
+            ),
+            marker_line=dict(width=1, color="black")
+        ),
+        layout=go.Layout(
+            xaxis=dict(title="Valores"),
+            yaxis=dict(title="Cantidad"),
+            title={
+                "text": "Histograma",
+                }
+        )
+    )
+
+    return fig
