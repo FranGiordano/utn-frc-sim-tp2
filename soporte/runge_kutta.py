0a2654d9ea516c863deaef4b379271be27bbbed2
JuaniMolina48 <juanirojo10@gmail.com>
2023-07-06 17:20:19 -0300

    Se añade la forma de resolver el segundo y tercer runge kutta

--- soporte/runge_kutta.py
+++ soporte/runge_kutta.py
@@ -28,3 +28,66 @@ def cuando_detiene(a, b, h):
         f_a = f_s
 
     return filas, f_a[7]
+
+
+def detencion_cliente(l,  h):
+    f_a, f_s = [0] * 8
+    filas = []
+
+    # Fila inicial
+    f_a[0] = 1
+    f_a[1] = l
+    f_a[2] = - (f_a[1] / (0.8*f_a[0]**2)) - f_a[1]
+    f_a[3] = - ((f_a[1] + (h/2) * f_a[2]) / (0.8 * (f_a[0] + (h/2)) ** 2)) - f_a[1] + (h/2) * f_a[2]
+    f_a[4] = - ((f_a[1] + (h/2) * f_a[3]) / (0.8 * (f_a[0] + (h/2)) ** 2)) - f_a[1] + (h/2) * f_a[3]
+    f_a[5] = - ((f_a[1] + h * f_a[4]) / (0.8 * (f_a[0] + h) ** 2)) - f_a[1] + h * f_a[4]
+    f_a[6] = f_a[0] + h
+    f_a[7] = f_a[1] + (h / 6) * (f_a[2] + 2 * f_a[3] + 2 * f_a[4] + f_a[5])
+
+    filas.append(f_a)
+
+    while abs(f_a[7] - f_a[1]) < 1:
+        f_s[0] = f_a[6]
+        f_s[1] = f_a[7]
+        f_s[2] = - (f_s[1] / (0.8 * f_s[0] ** 2)) - f_s[1]
+        f_s[3] = - ((f_s[1] + (h / 2) * f_s[2]) / (0.8 * (f_s[0] + (h / 2)) ** 2)) - f_s[1] + (h / 2) * f_s[2]
+        f_s[4] = - ((f_s[1] + (h / 2) * f_s[3]) / (0.8 * (f_s[0] + (h / 2)) ** 2)) - f_s[1] + (h / 2) * f_s[3]
+        f_s[5] = - ((f_s[1] + h * f_s[4]) / (0.8 * (f_s[0] + h) ** 2)) - f_s[1] + h * f_s[4]
+        f_s[6] = f_s[0] + h
+        f_s[7] = f_s[1] + (h / 6) * (f_s[2] + 2 * f_s[3] + 2 * f_s[4] + f_s[5])
+
+        filas.append(f_s)
+        f_a = f_s
+
+    return filas, f_a[7]
+
+def detencion_servidor(s, h):
+    f_a, f_s = [0] * 8
+    filas = []
+
+    # Fila inicial
+    f_a[0] = 0
+    f_a[1] = s
+    f_a[2] = (0.2 * f_a[1]) + 3 - f_a[0]
+    f_a[3] = (0.2 * (f_a[1] + (h/2) * f_a[2])) + 3 - (f_a[0] + (h/2))
+    f_a[4] = (0.2 * (f_a[1] + (h/2) * f_a[3])) + 3 - (f_a[0] + (h/2))
+    f_a[5] = (0.2 * (f_a[1] + (h/2) * f_a[4])) + 3 - (f_a[0] + (h/2))
+    f_a[6] = f_a[0] + h
+    f_a[7] = f_a[1] + (h / 6) * (f_a[2] + 2 * f_a[3] + 2 * f_a[4] + f_a[5])
+
+    filas.append(f_a)
+
+    while abs(f_a[7] - f_a[1]) < 1:
+        f_s[0] = f_a[6]
+        f_s[1] = f_a[7]
+        f_s[2] = (0.2 * f_s[1]) + 3 - f_a[0]
+        f_s[3] = (0.2 * (f_s[1] + (h / 2) * f_s[2])) + 3 - (f_s[0] + (h / 2))
+        f_s[4] = (0.2 * (f_s[1] + (h / 2) * f_s[3])) + 3 - (f_s[0] + (h / 2))
+        f_s[5] = (0.2 * (f_s[1] + (h / 2) * f_s[4])) + 3 - (f_s[0] + (h / 2))
+        f_s[6] = f_s[0] + h
+        f_s[7] = f_s[1] + (h / 6) * (f_s[2] + 2 * f_s[3] + 2 * f_s[4] + f_s[5])
+
+        filas.append(f_s)
+        f_a = f_s
+
+    return filas, f_a[7]
